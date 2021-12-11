e0b54a4f700e3628dd95b44ac13f6392b9669346
rafameureer <rafameureer@gmail.com>
2021-12-11 16:29:49 -0300

    Criando pagina de login

--- dragon-front/src/app/login/login.component.ts
+++ dragon-front/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,7 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  public loginForm: FormGroup;
+  public isPasswordVisible = false;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      user: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {}
+
+  public changePasswordVisibility(): void {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
 }
--- dragon-front/src/app/login/login.module.ts
+++ dragon-front/src/app/login/login.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login.component';
 
@@ -8,6 +8,6 @@ const routes: Routes = [{ path: '', component: LoginComponent }];
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), FormsModule]
+  imports: [CommonModule, RouterModule.forChild(routes), FormsModule, ReactiveFormsModule]
 })
 export class LoginModule {}
