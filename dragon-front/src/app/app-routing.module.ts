28681de4ab9a2382242d4ffc25cd7e312ea80289
rafameureer <rafameureer@gmail.com>
2021-12-18 15:21:07 -0300

    Auth guard e home

--- dragon-front/src/app/app-routing.module.ts
+++ dragon-front/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./login/login.module').then((m) => m.LoginModule) }];
+const routes: Routes = [
+  // { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', component: HomeComponent },
+  { path: 'login', loadChildren: () => import('./login/login.module').then((m) => m.LoginModule) },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
